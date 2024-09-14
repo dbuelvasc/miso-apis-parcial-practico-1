e8aff257cb22095eb891e6c421a427ab7fd1e7eb
dbuelvasc <d.buelvasc@uniandes.edu.co>
2024-09-14 12:17:38 -0400

    create tienda service

--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Producto } from './entities/producto.entity';
 
 @Injectable()
-export class ProductoService {}
+export class ProductoService {
+  constructor(
+    @InjectRepository(Producto)
+    private productoRepository: Repository<Producto>,
+  ) {}
+
+  async findAll(): Promise<Producto[]> {
+    return this.productoRepository.find();
+  }
+
+  async findOne(id: number): Promise<Producto> {
+    return this.productoRepository.findOneOrFail({ where: { id } });
+  }
+
+  async create(producto: Producto): Promise<Producto> {
+    this.validateProductType(producto.tipo);
+    return this.productoRepository.save(producto);
+  }
+
+  async update(id: number, producto: Producto): Promise<Producto> {
+    this.validateProductType(producto.tipo);
+    await this.productoRepository.update(id, producto);
+    return this.findOne(id);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.productoRepository.delete(id);
+  }
+
+  private validateProductType(tipo: string): void {
+    if (tipo !== 'Perecedero' && tipo !== 'No perecedero') {
+      throw new BadRequestException('Tipo de producto inválido');
+    }
+  }
+}
